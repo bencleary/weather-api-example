ee716e6bfb4521f407e52cfc7742eeb08121e3a2
Ben Cleary <benjamin.joel.cleary@gmail.com>
2018-12-23 16:30:43 +0000

    Initial Commit

---
+++ weather_api/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import WeatherAPI
+
+urlpatterns = [
+    path('weather', WeatherAPI.as_view(), name="weather-api")
+]
---
+++ weather_api/api/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views import View
+from .forms import WeatherForm
+from django.conf import settings
+import requests
+
+
+class WeatherAPI(View):
+
+    template_name = "app.html"
+    form = WeatherForm
+
+    def fetch_api_data(self, city):
+        try:
+            response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={settings.API_KEY}")
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            return e
+        
+    def get(self, request):
+        form = self.form()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = self.form(request.POST)
+        if form.is_valid():
+            api_response = self.fetch_api_data(form.cleaned_data['city'])
+        else:
+            api_response = None
+        return render(request, self.template_name, {'form': form, 'api_response': api_response})
